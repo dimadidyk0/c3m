1709780996f0a36058a64a69a36d2258ff064b5d
Dima <dimadidyk0@gmail.com>
2016-12-28 11:44:32 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+	// portalItem1.onclick = showHidden1;
+	// portalItem2.onclick = showHidden2;
+	// portalItem3.onclick = showHidden3;
+	// portalItem4.onclick = showHidden4;
+	// portalItem5.onclick = showHidden5;
+
+	// function showHidden1 () {
+	// 	if (portalHidden1.classList.contains("portal__item-vissible")) {
+	// 		portalHidden1.classList.remove("portal__item-vissible");
+	// 	}
+	// 		else portalHidden1.classList.add("portal__item-vissible");
+	// }
+
+
+	// FOR PORTAL PAGE if hidden text will exist
